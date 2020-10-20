b5d87156672320b2da00888c3b2735c948f26387
nutensen <marcnuez92@gmail.com>
2020-10-20 13:34:18 +0200

    config ok

--- server/config/config.js
+++ server/config/config.js
@@ -15,11 +15,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-// if(process.env.NODE_ENV === 'dev'){
-    // urlDB = 'mongodb://localhost:27017/cafe';
-// }else{
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/cafe';
+}else{
     urlDB = 'mongodb+srv://marcnut:marcnut29@cluster0.haex7.mongodb.net/cafe?authSource=admin&replicaSet=atlas-gy0c1n-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
-// }
+}
 
 
 process.env.URLDB = urlDB;
