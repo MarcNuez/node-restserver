dae4ec4ce5507cf4c255af88fa335c27b391130a
nutensen <marcnuez92@gmail.com>
2020-10-21 19:56:59 +0200

    login ok 2 intent

--- server/routes/login.js
+++ server/routes/login.js
@@ -45,7 +45,7 @@ app.post('/login', (req, res) => {
 
         let token = jwt.sign({
             usuario: usuarioDB
-        }, 'secret', { expiresIn: 60 * 60 * 24 * 30 });
+        }, process.env.SEED, { expiresIn: 60 * 60 * 24 * 30 });
 
         res.json({
             ok: true,
